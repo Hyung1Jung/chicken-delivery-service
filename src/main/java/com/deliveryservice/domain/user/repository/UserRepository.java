5027a23fb604678226c9bd6e7e4baac1d351abf7
Hyung1Jung <junghyungile@gmail.com>
2021-06-07 01:04:07 +0900

    [#1] feat : merge 관련 test

--- src/main/java/com/deliveryservice/domain/user/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.deliveryservice.domain.user.repository;
-
-import com.deliveryservice.domain.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByEmail(String email);
-}
