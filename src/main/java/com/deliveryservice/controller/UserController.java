786b46ce66b62229b206b4afb1d8251bea791543
Hyung1Jung <junghyungile@gmail.com>
2021-06-06 23:30:33 +0900

    [#1] feat : merge 관련 test

---
+++ src/main/java/com/deliveryservice/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.deliveryservice.controller;
+
+import static com.deliveryservice.controller.utils.ResponseConstants.CREATED;
+import com.deliveryservice.controller.dto.UserDto;
+import com.deliveryservice.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<Void> createUser(@Valid @RequestBody UserDto userDto) {
+        userService.saveUser(userDto);
+        return CREATED;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createUser2(@Valid @RequestBody UserDto userDto) {
+        userService.saveUser(userDto);
+        return CREATED;
+    }
+
+}
